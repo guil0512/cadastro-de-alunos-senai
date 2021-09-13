9b65747a4a3cb5248c38d3408599f35b87ce97a2
guil0512 <89713379+guil0512@users.noreply.github.com>
2021-09-13 20:11:31 -0300

    Add files via upload

---
+++ cadastro-alunos.js
@@ -0,0 +1,16 @@
+var quantidadeDeAlunos = 0;
+
+if (quantidadeDeAlunos == 0){
+    console.log("O número é 0");
+} else {
+    if (quantidadeDeAlunos %2 == 0)
+    console.log("O número " + quantidadeDeAlunos + " é par");
+     else if (quantidadeDeAlunos % 2 ==1)
+     console.log("O número " + quantidadeDeAlunos + " é impar");
+     }
+ 
+    
+
+
+
+ 
